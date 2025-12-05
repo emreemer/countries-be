9d147017c47424c32dbe154935539cd78f5f9820
emreemer <emreemer01@gmail.com>
2025-12-05 17:25:46 +0300

    proje yapisi olusturuldu

---
+++ src/test/java/com/emreemer/countries_be/CountriesBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.emreemer.countries_be;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CountriesBeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

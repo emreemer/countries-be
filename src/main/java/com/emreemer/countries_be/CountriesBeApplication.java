ecd5260862d13eaf601594d827e3639b3db99d88
emreemer <emreemer01@gmail.com>
2025-12-07 16:36:02 +0300

    logback-spring.xml is created

--- src/main/java/com/emreemer/countries_be/CountriesBeApplication.java
+++ src/main/java/com/emreemer/countries_be/CountriesBeApplication.java
@@ -1,12 +1,18 @@
 package com.emreemer.countries_be;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
 
 @SpringBootApplication
 public class CountriesBeApplication {
 
-	public static void main(String[] args) {
+	public static void main(String[] args)
+    {
+
+
 		SpringApplication.run(CountriesBeApplication.class, args);
 	}
 
